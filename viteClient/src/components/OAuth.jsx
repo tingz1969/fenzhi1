85ab37377c945ba92a0b3a063c46c4a59ca18753
zt <tingz1969@gmail.com>
2024-08-31 16:56:17 +0800

    update the header and make the profile page private

--- viteClient/src/components/OAuth.jsx
+++ viteClient/src/components/OAuth.jsx
@@ -12,7 +12,6 @@ export default function OAuth() {
             const provider = new GoogleAuthProvider();
             const auth = getAuth(app);
             const result = await signInWithPopup(auth, provider);
-            console.log(result);
             
             const res = await fetch('/api/auth/google', {
                 method: 'POST',
