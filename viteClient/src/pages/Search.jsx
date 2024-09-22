b48782521bb019a6c3e3b83cee5ee7914c70a57b
zt <tingz1969@gmail.com>
2024-09-22 14:12:32 +0800

    add show more listing

--- viteClient/src/pages/Search.jsx
+++ viteClient/src/pages/Search.jsx
@@ -15,6 +15,7 @@ export default function Search() {
     });
     const [loading, setLoading] = useState(false);
     const [listings, setListings] = useState([]);
+    const [showMore, setShowMore] = useState(false);
     console.log(listings);
     
     useEffect(() => {
@@ -41,9 +42,15 @@ export default function Search() {
        
         const fetchListings = async () => {
             setLoading(true);
+            setShowMore(false);
             const searchQuery = urlParams.toString();
             const res = await fetch(`/api/listing/get?${searchQuery}`);
             const data = await res.json();
+            if (data.length > 8) {
+                setShowMore(true);
+            } else {
+                setShowMore(false);
+            }
             setListings(data);
             setLoading(false);
         };
@@ -83,6 +90,20 @@ export default function Search() {
         urlParams.set('order', sidebardata.order);
         const searchQuery = urlParams.toString();
         navigate(`/search?${searchQuery}`);
+    };
+
+    const onShowMoreClick = async () => {
+        const numberOfListings = listings.length;
+        const startIndex = numberOfListings;
+        const urlParams = new URLSearchParams(location.search);
+        urlParams.set('startIndex', startIndex);
+        const searchQuery = urlParams.toString();
+        const res = await fetch(`/api/listing/get?${searchQuery}`);
+        const data = await res.json();
+        if (data.length < 9) {
+            setShowMore(false);
+        }
+        setListings([...listings, ...data]);
     };
 
     return (
@@ -154,6 +175,10 @@ export default function Search() {
                             <ListingItem key={listing._id} listing={listing} />
                         )
                     )}
+
+                    {
+                        showMore && (<button onClick={onShowMoreClick} className='text-green-700 hover:underline p-7 text-center w-full'>Show more</button>)
+                    }
                 </div>
             </div>
         </div>
